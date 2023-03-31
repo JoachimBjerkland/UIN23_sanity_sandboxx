a114a4ef7d94e0263aed1dc3c5c21da1aadde1fe
Joachim Bjerkland <Bjerkland2011@hotmail.no>
2023-03-31 09:55:05 +0200

    Sanity

--- frontend/src/pages/CategoriesPage.js
+++ frontend/src/pages/CategoriesPage.js
@@ -1,3 +1,31 @@
+import {useEffect, useState} from "react";
+import { fetchAllCategories } from "../lib/sanity/categoryServices";
+import { Link } from "react-router-dom"
+
 export default function CategoriesPage() {
-    return <h1>Alle kategorier</h1>
+    //lage en state for å lagre kategoriene
+    const [cats, setCats] = useState(null)
+
+
+    //Kalle categorySerbice asynkront
+    async function getCategories() {
+        const data = await fetchAllCategories()
+        setCats(data)
+    }
+
+    //Kjøre getCategories når components lastes: 
+    useEffect(() => {
+        getCategories()
+    }, [])
+    console.log(cats)
+
+
+    return (
+        <>
+    <h1>Alle kategorier</h1>
+    <ul>
+        {cats?.map((c, i) => <li><Link to={c.category_slug.current}>{c.category_title}</Link></li>)}
+    </ul>
+    </>
+    ) 
 }
